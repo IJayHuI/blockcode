b4dd2cd49ca2c9dc6ba914e6e345040f7da7a9dd
IJayHuI <2949763264@qq.com>
2025-11-04 12:38:43 +0800

    chore

--- src/views/mains/BcMain.vue
+++ src/views/mains/BcMain.vue
@@ -4,7 +4,7 @@
 <template>
   <n-layout has-sider position="absolute">
     <n-layout-sider bordered width="200" content-class="flex flex-col"><bc-menu /></n-layout-sider>
-    <n-layout>
+    <n-layout content-class="p-2">
       <router-view></router-view>
     </n-layout>
   </n-layout>
