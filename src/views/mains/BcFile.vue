0cffc673f98f4e73c9dd65935d5d7724bcd522c9
IJayHuI <2949763264@qq.com>
2025-11-06 19:07:14 +0800

    update

--- src/views/mains/BcFile.vue
+++ src/views/mains/BcFile.vue
@@ -1,2 +1,14 @@
-<script setup></script>
-<template>files</template>
+<script setup>
+  import BcCreateAndUpload from '@/components/file/BcCreateAndUpload.vue'
+  import BcFileList from '@/components/file/BcFileList.vue'
+</script>
+<template>
+  <p class="text-3xl font-bold mb-2">我的文件</p>
+  <div class="flex flex-col gap-2">
+    <div><bc-create-and-upload /></div>
+    <div>
+      <p class="text-2xl font-bold">文件列表</p>
+      <bc-file-list />
+    </div>
+  </div>
+</template>
